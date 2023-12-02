d9922616c36a488bfa33b5fb16dc88923e01c2e6
Satish Y C <sryc123@gmail.com>
2023-12-02 22:05:08 +0530

    Removing Map Logic from RomanToInteger.java

--- src/main/java/org/dsa/RomanToInteger.java
+++ src/main/java/org/dsa/RomanToInteger.java
@@ -5,14 +5,6 @@ import java.util.Map;
 
 public class RomanToInteger {
     public static int romanToInt(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        map.put('I',1);
-        map.put('V',5);
-        map.put('X',10);
-        map.put('L',50);
-        map.put('C',100);
-        map.put('D',500);
-        map.put('M',1000);
         int sum =0 ;
         int n = s.length();
         int previous = 0;
