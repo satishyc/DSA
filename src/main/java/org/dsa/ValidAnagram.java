06ce96be77e315c01791804bbd9b80f6f3871103
Satish Y C <sryc123@gmail.com>
2023-12-05 00:30:24 +0530

    Adding Solution for the problem ValidAnagram

---
+++ src/main/java/org/dsa/ValidAnagram.java
@@ -0,0 +1,32 @@
+package org.dsa;
+
+public class ValidAnagram {
+    /*
+    * Problem Link https://leetcode.com/problems/valid-anagram/description/
+    * */
+
+    public static boolean isAnagram(String s, String t) {
+            if(s.length()!=t.length()){
+                return false;
+            }
+            char[] counts = new char[26];
+            for(char ch : s.toCharArray()){
+                counts[ch-'a']++;
+            }
+            for(char ch : t.toCharArray()){
+                counts[ch-'a']--;
+            }
+            for(int i=0;i<26;i++){
+                if(counts[i]!=0)
+                    return false;
+            }
+            return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram","nagaram"));
+        System.out.println(isAnagram("cat","cata"));
+
+    }
+
+}
