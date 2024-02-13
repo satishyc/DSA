210828598290a95e1a0318da36aa45b0f9941d55
Satish Y C <sryc123@gmail.com>
2024-02-13 23:24:21 +0530

    Adding Optimal Solution for MajorityElement and MajorityElement2

--- src/main/java/org/dsa/arrays/MajorityElement.java
+++ src/main/java/org/dsa/arrays/MajorityElement.java
@@ -20,9 +20,35 @@ public class MajorityElement {
         return -1;
 
     }
+    public static int majorityElement2(int[] values){
+        int count = 0;
+        int element = Integer.MIN_VALUE;
+        for(int val : values){
+            if(count==0){
+                count++;
+                element=val;
+            }
+            else if(element==val){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        int count2=0;
+        for(int val : values){
+            if(val==element)
+                count2++;
+        }
+        if(count2>(values.length/2))
+            return element;
+        return -1;
+
+    }
 
     public static void main(String[] args) {
-        int[] values = new int[]{2,2,1,1,1,2,2};
+        int[] values = new int[]{1,2,2,2,1,1,1,2,2,1};
         System.out.println(majorityElement(values));
+        System.out.println(majorityElement2(values));
     }
 }
--- src/main/java/org/dsa/arrays/MajorityElement2.java
+++ src/main/java/org/dsa/arrays/MajorityElement2.java
@@ -25,7 +25,7 @@ public class MajorityElement2 {
         }
         return result;
     }
-    public static List<Integer> majorityElementOptimal(int[] nums){
+    public static List<Integer> majorityElementOptimal2(int[] nums){
         List<Integer> result = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
@@ -49,11 +49,53 @@ public class MajorityElement2 {
 
         return result;
     }
+    public static List<Integer> majorityElementOptimal(int[] nums){
+        int cnt1 =0;
+        int cnt2=0;
+        int ele1 = Integer.MIN_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+        for(int num : nums){
+            if(cnt1==0 && ele2!=num){
+                ele1=num;
+                cnt1++;
+            }
+            else if(cnt2==0 && ele1!=num){
+                ele2=num;
+                cnt2++;
+            }
+            else if(num==ele1) cnt1++;
+            else if(num==ele2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0;
+        cnt2=0;
+        for(int num : nums){
+            if(num==ele1) cnt1++;
+            if(num==ele2) cnt2++;
+        }
+        List<Integer> result = new ArrayList<>();
+        if(cnt1>=(nums.length/3)+1)
+            result.add(ele1);
+        if(cnt2>=(nums.length/3)+1)
+            result.add(ele2);
+        return result;
+
+
+
+    }
 
     public static void main(String[] args) {
         List<Integer> res;
-        int[] nums = new int[]{1};
+        int[] nums = new int[]{2,2,3};
         res = majorityElement(nums);
+        for(int r : res){
+            System.out.print(" "+r);
+        }
+        System.out.println();
+        res = majorityElementOptimal2(nums);
         for(int r : res){
             System.out.print(" "+r);
         }
