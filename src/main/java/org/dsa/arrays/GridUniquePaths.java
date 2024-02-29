8fc9919b7f0cc69aa4076bb4a4f4480983942991
Satish Y C <sryc123@gmail.com>
2024-02-29 23:59:37 +0530

    Adding solution for GridUniquePaths

---
+++ src/main/java/org/dsa/arrays/GridUniquePaths.java
@@ -0,0 +1,51 @@
+package org.dsa.arrays;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Problem Link <a href="https://leetcode.com/problems/unique-paths/description/"></a>
+ * */
+public class GridUniquePaths {
+    public static int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return recursive(dp,m,n,0,0);
+
+    }
+    private static int  recursive(int dp[][],int m,int n,int x,int y){
+            if(x==m-1 && y==n-1) return 1;
+            else if(x>=m || y>=n) return 0;
+            if(dp[x][y]!=-1) return dp[x][y];
+            else return dp[x][y]=recursive(dp,m,n,x+1,y)+recursive(dp,m,n,x,y+1);
+
+    }
+
+    public static void main(String[] args) {
+        int m=5,n=10;
+
+        System.out.println(uniquePaths(m,n));
+        System.out.println(optimal(m,n));
+    }
+    public static int optimal(int m,int n){
+        int N=m+n-2;
+        int r=n-1;
+        double res = 1.0;
+        for(int i=1;i<=r;i++){
+            res=res*(N-r+i)/i;
+        }
+        return (int)res;
+    }
+    public static BigInteger factorial(int n){
+        BigInteger result = BigInteger.valueOf(1);
+        for(int i=1;i<=n;i++){
+            result=result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+
+}
